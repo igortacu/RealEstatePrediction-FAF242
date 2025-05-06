f116119265b9eedc809d9ec21e70ff3d5a349590
Daniel <danielcanter2013@gmail.com>
2025-05-06 16:48:56 +0300

    docker script update

--- Backend/Build/docker_up.sh
+++ Backend/Build/docker_up.sh
@@ -4,5 +4,4 @@ docker rm -f api-server
 
 docker build -t test-server .
 
-docker run -d -p 5000:5000 test-server --name api-server
-
+docker run --name api-server --network npm_default -d  test-server
